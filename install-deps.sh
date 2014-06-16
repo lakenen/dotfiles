ab630e231cdb687b1ea797d6ade20c28ec2ad765
Cameron Lakenen <lakenen@box.com>
2014-06-16 13:28:46 -0700

    Fix z (install-deps.sh)

--- install-deps.sh
+++ install-deps.sh
@@ -1,8 +1,10 @@
+#!/bin/bash
+
 # up to you (me) if you want to run this as a file or copy paste at your leisure
 
 # https://github.com/rupa/z
 mkdir -p ~/code/z
-curl https://raw.github.com/rupa/z/master/z.sh > ~/code/z/z.sh
+curl -L https://raw.githubusercontent.com/rupa/z/master/z.sh > ~/code/z/z.sh
 
 # add this to the bash_profile file if it aint there.
-#source ~/code/z/z.sh
+source ~/code/z/z.sh
